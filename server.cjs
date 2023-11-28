22b3d6fa001d13373133a4b9a60926bd81b6a717
Jaylen <jmowens90@gmail.com>
2023-11-28 10:12:56 -0500

    changed the code to reflect the workshop examples

--- server.cjs
+++ server.cjs
@@ -4,17 +4,21 @@ console.log(pets)
 
 const app = express();
 
-const PORT = 8080;
+const PORT = 3000;
 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
+app.get('/api/v1/pets/owner', (req, res) => { 
+    const {owner} = req.query; 
+    const filtered = pets.filter(pet => pet.owner.toLowerCase() === owner.toLowerCase());
+    res.send(filtered); 
+});
+//http://localhost:3000/api/v1/pets/owner?owner=john  this is how you type in the query to filter a few new options
+
 app.get('/api/v1/pets', (req, res) => {res.send(pets); });
 
-app.get('/api/v1/pets/:name', (req, res) => { const pet = req.params.name; res.send(pet); });
+app.get('/api/v1/pets/:name', (req, res) => { 
+const {name} = req.params; 
+const filtered = pets.filter(pet => pet.name.toLowerCase() === name.toLowerCase());
+res.send(filtered); });
 
-app.get('/api/v1/pets/owner/:owner', (req, res) => { 
-    const owner = req.params.owner; 
-    const pet = pets.filter(pet => pet.owner === owner);
-    const petNames = pet.map(pet => pet.name);
-    res.send(petNames); 
-})
